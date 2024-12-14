7f414745477fb657932835c3605eb2974e803aab
Imran1128 <138041710+Imran1128@users.noreply.github.com>
2024-12-14 21:53:50 +0600

    nb

---
+++ Web_Form/Data/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_Form.Data
+{
+    public class DbContext:IdentityDbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options): base(options)
+        {
+            
+        }
+    }
+}
